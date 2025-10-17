c3b5b3d5ca385a1a277e65385adbba41a5247c72
Amanuel Wubishet <162797908+BIGANUEL@users.noreply.github.com>
2025-10-17 20:46:13 +0300

    Time: 2236 ms (68.59%) | Memory: 17.9 MB (62.67%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [float('inf')] * (n + 1)
+        dp[0] = 0
+
+        squares = [i * i for i in range(1, int(n ** 0.5) + 1)]
+
+        for i in range(1, n + 1):
+            for sq in squares:
+                if sq > i:
+                    break
+                dp[i] = min(dp[i], dp[i - sq] + 1)
+
+        return dp[n]
